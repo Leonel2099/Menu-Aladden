0bcfec1909d3be0005de0f900f0ee5db44b491cc
Leonel2099 <ezequielleonel1@gmail.com>
2023-09-09 14:15:10 -0300

    Se importa el componente del footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Footer from "./layouts/Footer/Footer";
 import NavBar from "./layouts/NavBar/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css"
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
+      <Footer></Footer>
     </>
   );
 }
