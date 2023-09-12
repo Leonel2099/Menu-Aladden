e17aa677c3fcabd039a3cc23d441c3a17ef83ccc
Leonel2099 <ezequielleonel1@gmail.com>
2023-09-12 00:16:23 -0300

    Se linkeo las views del componente TabsView

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "../Home/Home.css";
 import Hero from "./views/Hero/Hero";
+import TabsView from "./views/TabsView/TabsView";
+import TabViewAbout from "./views/TabsView/TabViewAbout/TabViewAbout";
+import TabViewMenu from "./views/TabsView/TabViewMenu/TabViewMenu";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="home">
       <Hero></Hero>
+      <BrowserRouter>
+        <TabsView></TabsView>
+        <Routes>
+          <Route exact path="/tabViewAbout" element={<TabViewAbout></TabViewAbout>}></Route>
+          <Route exact path="/tabViewMenu" element={<TabViewMenu></TabViewMenu>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
