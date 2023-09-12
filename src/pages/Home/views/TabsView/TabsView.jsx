282a593650d69151b8b44b72c8b7eb563d94190a
Leonel2099 <ezequielleonel1@gmail.com>
2023-09-12 00:10:15 -0300

    Se creo y se maqueto el componente TabsViews

---
+++ src/pages/Home/views/TabsView/TabsView.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
+
+const TabsView = () => {
+  return (
+    <Nav variant="tabs">
+      <Nav.Item>
+        <NavLink className="nav-link text-white" to={"/tabViewAbout"}>
+          Sobre Nosotros
+        </NavLink>
+      </Nav.Item>
+      <Nav.Item>
+        <NavLink className="nav-link text-white" to={"/tabViewMenu"}>
+          Menu
+        </NavLink>
+      </Nav.Item>
+    </Nav>
+  );
+};
+
+export default TabsView;
