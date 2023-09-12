16ece5effac399bc97b9cf6cd8edf104c66c8b20
Leonel2099 <ezequielleonel1@gmail.com>
2023-09-12 00:12:32 -0300

    Se creo un componente TabViewAbout

---
+++ src/pages/Home/views/TabsView/TabViewAbout/TabViewAbout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { CardGroup } from "react-bootstrap";
+
+const TabViewAbout = () => {
+  return <div className="text-white">TabViewAbout</div>;
+};
+
+export default TabViewAbout;
