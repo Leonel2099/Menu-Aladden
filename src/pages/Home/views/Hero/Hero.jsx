d6d73e02d5ed81cca3acd00658963d1d679a2edd
Leonel2099 <ezequielleonel1@gmail.com>
2023-09-16 12:03:31 -0300

    Se comento el import del banner

--- src/pages/Home/views/Hero/Hero.jsx
+++ src/pages/Home/views/Hero/Hero.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import banner from "../../../../assets/img/banner.gif";
+// import banner from "../../../../assets/img/banner.gif";
 import logoPedido from "../../../../assets/icons/pedido.png";
 import logoReservasion from "../../../../assets/icons/reservacion.png";
 import "../Hero/Hero.css";
 const Hero = () => {
   return (
     <div className=" position-relative mb-4">
-      <img className="banner" src={banner} alt="video cocinando un kebah" />
+      <img className="banner"  alt="video cocinando un kebah" />
       <div className="btnBanner d-flex">
         <Button variant="success" className="btnHero m-2 d-flex ">
           <img className="logoBtn" src={logoPedido} alt="" />
