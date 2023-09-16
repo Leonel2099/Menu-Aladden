489951e0cddc027fced14797fdc8c0439b01fcc3
Leonel2099 <ezequielleonel1@gmail.com>
2023-09-16 12:06:20 -0300

    Se soluciono el error de broswer router

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,19 +4,17 @@ import Hero from "./views/Hero/Hero";
 import TabsView from "./views/TabsView/TabsView";
 import TabViewAbout from "./views/TabsView/TabViewAbout/TabViewAbout";
 import TabViewMenu from "./views/TabsView/TabViewMenu/TabViewMenu";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="home mainSection">
       <Hero></Hero>
-      <BrowserRouter>
         <TabsView></TabsView>
         <Routes>
           <Route exact path="/" element={<TabViewAbout></TabViewAbout>}></Route>
           <Route exact path="/tabViewMenu" element={<TabViewMenu></TabViewMenu>}></Route>
         </Routes>
-      </BrowserRouter>
     </div>
   );
 };
