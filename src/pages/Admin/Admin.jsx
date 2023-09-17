d1750b8f618764de034181c7c82c1cf3f96c91ce
Leonel2099 <ezequielleonel1@gmail.com>
2023-09-16 22:10:06 -0300

    Se maqueto la pagina Admin , con todos los tab enrrutados

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import TabsViewAdmin from "./TabsViewAdmin/TabsViewAdmin";
+import TabViewUsuario from "../../pages/Admin/TabsViewAdmin/TabViewUsuario/TabViewUsuario";
+import TabViewPlatos from "../../pages/Admin/TabsViewAdmin/TabViewPlatos/TabViewPlatos";
+import TabViewPedidos from "../../pages/Admin/TabsViewAdmin/TabViewPedidos/TabViewPedidos";
 const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
+    <div className="mainSection">
+      <section>
+        <h2 className="text-center text-white display-2">Administrador</h2>
+        <TabsViewAdmin></TabsViewAdmin>
+        <Routes>
+          <Route exact path="/" element={<TabViewUsuario />} />
+          <Route exact path="/tabViewDishes" element={<TabViewPlatos />} />
+          <Route exact path="/tabViewOrders" element={<TabViewPedidos />} />
+        </Routes>
+      </section>
+    </div>
+  );
+};
 
-export default Admin
+export default Admin;
