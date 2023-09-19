d18c07c7f866a0d5790c38cbdd6fa0d3f3ae3439
Leonel2099 <ezequielleonel1@gmail.com>
2023-09-19 18:33:43 -0300

    Se cambio el boton login , por el nuevo componente modal del login

--- src/layouts/NavBar/NavBar.jsx
+++ src/layouts/NavBar/NavBar.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Button, Container, Nav, NavLink, Navbar } from "react-bootstrap";
+import { Container, Navbar } from "react-bootstrap";
 import "./NavBar.css";
 import logo from "../../assets/img/logoAladden.png";
-import ButtonLogin from "../../components/Buttons/ButtonLogin/ButtonLogin";
 import ButtonRegister from "../../components/Buttons/ButtonRegister/ButtonRegister";
+import LoginModal from "../../components/Overlay/LoginModal/LoginModal";
 const NavBar = () => {
   return (
     <>
       <Navbar expand="lg">
         <Container className="justify-content-center justify-content-lg-between justify-content-md-around">
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="/">
             <img src={logo} className="d-inline-block align-center logo img-fluid" />
           </Navbar.Brand>
           <div className="d-flex">
-            <ButtonLogin></ButtonLogin>
+            <LoginModal></LoginModal>
             <ButtonRegister></ButtonRegister>
           </div>
         </Container>
