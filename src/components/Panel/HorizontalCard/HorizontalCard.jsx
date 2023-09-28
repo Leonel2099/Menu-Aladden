8f7b3805b0df113075b3f1dbcea82854d79627e4
Leonel2099 <ezequielleonel1@gmail.com>
2023-09-27 21:50:08 -0300

    Se maqueto las card horizontales con sus respectivos estilos

---
+++ src/components/Panel/HorizontalCard/HorizontalCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Button, Card, CardImg } from "react-bootstrap";
+import "../HorizontalCard/HorizontalCard.css"
+const HorizontalCard = () => {
+  return (
+    <Card className='text-white cardProductoPedidos'>
+        <section className="d-flex justify-content-between jusjustify-content-md-evenly text-center flex-column-reverse flex-md-row">
+          <div>
+            <Card.Body id='cardHorizontal'>
+              <Card.Title className='fs-1'>Shawarma</Card.Title>
+              <Card.Text>
+                <div className='fs-5'>
+                  <p className='m-0 text-start'>Espesificaciones</p>
+                  <div>
+                    <ul>
+                      <li>sin picante</li>
+                      <li>salsa aparte </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="d-flex flex-row justify-content-around">
+                  <p className='m-0 me-3'>Precio: $400</p>
+                  <p className='m-0 me-3'>Cantidad: x 3</p>
+                </div>
+              </Card.Text>
+              <div className='text-start'>
+                <Button>Editar Cantidad</Button>
+              </div>
+            </Card.Body>
+          </div>
+          <div>
+            <CardImg className="imgProductoPedido img-fluid" src='https://images.pexels.com/photos/2955819/pexels-photo-2955819.jpeg'/>
+          </div>
+        </section>
+      </Card>
+  )
+}
+
+export default HorizontalCard
