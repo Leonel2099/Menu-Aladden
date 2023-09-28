f0130a9abfab083586caa394841aeb2f8eabeb1e
Leonel2099 <ezequielleonel1@gmail.com>
2023-09-27 21:44:11 -0300

    Se cambio el boton por un link , y se puso la ruta ah pedidos

--- src/pages/Home/views/Hero/Hero.jsx
+++ src/pages/Home/views/Hero/Hero.jsx
@@ -3,16 +3,17 @@ import { Button } from "react-bootstrap";
 // import banner from "../../../../assets/img/banner.gif";
 import logoPedido from "../../../../assets/icons/pedido.png";
 import logoReservasion from "../../../../assets/icons/reservacion.png";
+import { Link } from "react-router-dom"
 import "../Hero/Hero.css";
 const Hero = () => {
   return (
     <div className=" position-relative mb-4">
       <img className="banner"  alt="video cocinando un kebah" />
       <div className="btnBanner d-flex">
-        <Button variant="success" className="btnHero m-2 d-flex ">
+        <Link variant="success" to={"/pedidos"} className="btn btnHero m-2 d-flex ">
           <img className="logoBtn" src={logoPedido} alt="" />
           <p className="m-0">Pedidos</p>
-        </Button>
+        </Link>
         <Button variant="success" className="btnHero m-2 d-flex ">
           <img className="logoBtn" src={logoReservasion} alt="" />
           <p className="m-0">Reservaciones</p>
