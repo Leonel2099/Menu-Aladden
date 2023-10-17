e60b4296f359def1601be58f56e68210bc2c1af3
Leonel2099 <ezequielleonel1@gmail.com>
2023-10-17 12:00:13 -0300

    Se cambio el icono de registro por un url

--- src/components/Buttons/ButtonRegister/ButtonRegister.jsx
+++ src/components/Buttons/ButtonRegister/ButtonRegister.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button } from "react-bootstrap";
-import logoResgistro from "../../../assets/icons/registerLogo.png"
 import "../ButtonRegister/ButtonRegister.css"
 
 const ButtonRegister = ({setShow}) => {
@@ -8,7 +7,7 @@ const ButtonRegister = ({setShow}) => {
   return (
     <Button onClick={handleShow} className="d-flex align-items-center" variant="success">
       <div className="me-1">
-        <img className="logoRegistro" src={logoResgistro} alt="logo de registro" />
+        <img className="logoRegistro" src="https://i.ibb.co/9cZvQjB/register-Logo.png" alt="logo de registro" />
       </div>
       <p className="m-0 p-0">Registrarse</p>
     </Button>
