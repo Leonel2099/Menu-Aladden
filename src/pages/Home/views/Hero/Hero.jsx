bdc9592604939a726bf1fd36a5a554fa88ce5bc0
Leonel2099 <ezequielleonel1@gmail.com>
2023-10-17 12:01:19 -0300

    Se cambio los iconso y el baner por un url

--- src/pages/Home/views/Hero/Hero.jsx
+++ src/pages/Home/views/Hero/Hero.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-// import banner from "../../../../assets/img/banner.gif";
-import logoPedido from "../../../../assets/icons/pedido.png";
-import logoReservasion from "../../../../assets/icons/reservacion.png";
 import { Link } from "react-router-dom"
 import "../Hero/Hero.css";
 const Hero = () => {
   return (
     <div className=" position-relative mb-4">
-      <img className="banner"  alt="video cocinando un kebah" />
+      <img className="banner" src="https://i.ibb.co/YBtnNZz/banner.gif" alt="video cocinando un kebah" />
       <div className="btnBanner d-flex">
         <Link variant="success" to={"/pedidos"} className="btn btnHero m-2 d-flex ">
-          <img className="logoBtn" src={logoPedido} alt="" />
+          <img className="logoBtn" src="https://i.ibb.co/KqkKb6D/pedido.png" alt="" />
           <p className="m-0">Pedidos</p>
         </Link>
         <Button variant="success" className="btnHero m-2 d-flex ">
-          <img className="logoBtn" src={logoReservasion} alt="" />
+          <img className="logoBtn" src="https://i.ibb.co/Stg1md9/reservacion.png" alt="" />
           <p className="m-0">Reservaciones</p>
         </Button>
       </div>
